4739f74807d2e6b5f0002b6fc3cc399968598bcc
Riley Stratton <riley.s.stratton@outlook.com>
2022-09-08 09:38:20 -0600

    added device prompt and touchscreen if on surface

--- nobara.sh
+++ nobara.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 
-# time and hostname
+# device (used to know if fstab or surface settings should be added)
+while [[ "$device" != "desktop" && "$device" != "laptop" && "$device" != "other" ]]; do
+    read -p "What device are you setting up? (DESKTOP, LAPTOP, or OTHER): " input
+    device=`echo $input | tr '[:upper:]' '[:lower:]'`
+    echo $device
+done
+
+
+# time
 sudo timedatectl set-local-rtc 0
 sudo timedatectl set-timezone America/Denver
-read -p "What would you like to set the hostname to? " name
+
+
+# hostname
+name="nobara-$device"
+# read -p "What would you like to set the hostname to? " name
 sudo hostnamectl hostname $name
 
 
@@ -18,23 +30,30 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 
 # brave
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+# sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf -y install brave-browser
 # open brave and set up settings sync
 
 
 # mega
+mkdir $HOME/mega
+
 cd $HOME/Downloads
 wget https://mega.nz/linux/repo/Fedora_36/x86_64/megasync-Fedora_36.x86_64.rpm
 sudo dnf install ./megasync-Fedora_36.x86_64.rpm
-mkdir $HOME/mega
 
 if [[ "$XDG_CURRENT_DESKTOP" == "KDE" ]]; then
     wget https://mega.nz/linux/repo/Fedora_36/x86_64/dolphin-megasync-Fedora_36.x86_64.rpm
     sudo dnf install -y ./dolphin-megasync-Fedora_36.x86_64.rpm
+
 elif [[ "$XDG_CURRENT_DESKTOP" == "GNOME" ]]; then
     wget https://mega.nz/linux/repo/Fedora_36/x86_64/nautilus-megasync-Fedora_36.x86_64.rpm
     sudo dnf install -y ./nautilus-megasync-Fedora_36.x86_64.rpm
+    
+elif [[ "$XDG_CURRENT_DESKTOP" == "i3" || "$XDG_CURRENT_DESKTOP" == "XFCE" ]]; then
+    wget https://mega.nz/linux/repo/Fedora_36/x86_64/thunar-megasync-Fedora_36.x86_64.rpm
+    sudo dnf install -y ./nautilus-megasync-Fedora_36.x86_64.rpm
+
 else
     echo "Unsupported Desktop Environment"
     echo "Please manually add File Manager Integration from https://mega.io/desktop"
@@ -43,30 +62,38 @@ fi
 
 
 # code
-cd $HOME
-mkdir code; cd code
-mkdir src; cd src
-mkdir github.com; cd github.com
-mkdir rilstrats 
+mkdir -p $HOME/code/src/github.com/rilstrats $HOME/code/src/github.com/byui-csa
 
 
 # fstab 
-read -p 'Would you like to add the vm and game drives? [Y/n]: ' input
-clean=`echo ${input:0:1} | tr '[:upper:]' '[:lower:]'`
+# read -p 'Would you like to add the vm and game drives? [Y/n]: ' input
+# fstab=`echo ${input:0:1} | tr '[:upper:]' '[:lower:]'`
 
-if [[ $clean == 'n' ]]; then
-    echo 'Drives not added'
-else
+# if [[ $fstab == 'n' ]]; then
+#     echo 'Drives not added'
+# else
+
+if [[ $device == 'desktop' ]]; then
     echo "UUID=29071D3603B7A859   /mnt/vm/      ntfs-3g    uid=1000,gid=1000,rw,user,exec,umask=000 0 0
     UUID=5A42FF7E42FF5D67   /mnt/games/     ntfs-3g uid=1000,gid=1000,rw,user,exec,umask=000 0 0" | sudo tee -a /etc/fstab
     echo 'Drives added'
-    echo 
 fi
 
 
 # wally (for moonlander keyboard)
-sudo cp other/50-wally.rules /etc/udev/rules.d/50-wally.rules
-sudo groupadd plugdev; sudo usermod -aG plugdev $USER
+if [[ $device == 'desktop' ]]; then
+    sudo cp other/50-wally.rules /etc/udev/rules.d/50-wally.rules
+    sudo groupadd plugdev; sudo usermod -aG plugdev $USER
+fi
+
+
+# surface
+if [[ $device == 'laptop' ]]; then
+    sudo dnf config-manager --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+    sudo dnf install --allowerasing iptsd libwacom-surface
+    sudo systemctl enable iptsd
+    # also requires a reboot
+fi
 
 
 # .dotfiles 
